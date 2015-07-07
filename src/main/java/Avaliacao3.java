d1a044c75c93e31ff1d49825621db8aad893aac5
a1590677 <a1590677@localhost>
2015-07-07 11:47:04 -0300

    first commit

--- src/main/java/Avaliacao3.java
+++ src/main/java/Avaliacao3.java
@@ -2,21 +2,79 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
 import utfpr.ct.dainf.if62c.avaliacao.Lancamento;
+import utfpr.ct.dainf.if62c.avaliacao.LancamentoComparator;
+import utfpr.ct.dainf.if62c.avaliacao.ProcessaLancamentos;
 
 /**
- * IF62C Fundamentos de Programação 2
- * Avaliação parcial.
- * @author 
+ * IF62C Fundamentos de Programação 2 Avaliação parcial.
+ *
+ * @author
  */
 public class Avaliacao3 {
- 
-    public static void main(String[] args) throws FileNotFoundException, IOException {
 
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        Scanner scanner = new Scanner(System.in);
+        String nome1;
+        System.out.println("Digite o caminho:  ");
+        nome1 = scanner.next();
+        ProcessaLancamentos p = new ProcessaLancamentos(nome1);
+        List<Lancamento> l1 = p.getLancamentos();
+        l1.sort(new LancamentoComparator());
+        System.out.print("Digite o numero da conta: ");
+        int num;
+        if (scanner.hasNextInt()) {
+            num = scanner.nextInt();
+        } else {
+            while (!scanner.hasNextInt()) {
+                System.out.println("Por favor, informe um valor numérico!");
+                scanner.next();
+            }
+            num = scanner.nextInt();
+        }
+        int flag = 0;
+        for (Lancamento l : l1) {
+            if (Objects.equals(l.getConta(), num)) {
+                flag = 1;
+            }
+        }
+
+        while (num != 0) {
+            if (flag == 1) {
+                exibeLancamentosConta(l1, num);
+            } else {
+                System.out.println("Conta inexistente");
+            }
+            System.out.print("Digite o numero da conta: ");
+            if (scanner.hasNextInt()) {
+                num = scanner.nextInt();
+            } else {
+                while (!scanner.hasNextInt()) {
+                    System.out.println("Por favor, informe um valor numérico!");
+                    scanner.next();
+                }
+                num = scanner.nextInt();
+            }
+            flag = 0;
+            for (Lancamento l : l1) {
+                if (Objects.equals(l.getConta(), num)) {
+                    flag = 1;
+                }
+
+            }
+
+        }
     }
-    
+
     public static void exibeLancamentosConta(List<Lancamento> lancamentos, Integer conta) {
 
+        for (Lancamento l : lancamentos) {
+            if (Objects.equals(l.getConta(), conta)) {
+                System.out.println(l);
+            }
+        }
     }
- 
+
 }
--- src/main/java/utfpr/ct/dainf/if62c/avaliacao/ProcessaLancamentos.java
+++ src/main/java/utfpr/ct/dainf/if62c/avaliacao/ProcessaLancamentos.java
@@ -3,7 +3,12 @@ package utfpr.ct.dainf.if62c.avaliacao;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -15,27 +20,58 @@ public class ProcessaLancamentos {
     private BufferedReader reader;
 
     public ProcessaLancamentos(File arquivo) throws FileNotFoundException {
-        throw new UnsupportedOperationException("Não implementado");
+        reader = new BufferedReader(new FileReader(arquivo));
     }
 
     public ProcessaLancamentos(String path) throws FileNotFoundException {
-        throw new UnsupportedOperationException("Não implementado");
+        reader = new BufferedReader(new FileReader(path));
     }
     
     private String getNextLine() throws IOException {
-        throw new UnsupportedOperationException("Não implementado");
-    }
+        String linha;
+        linha=reader.readLine();
+            return linha;
+        }
+    
     
     private Lancamento processaLinha(String linha) {
-        throw new UnsupportedOperationException("Não implementado");
+    Integer conta;
+    int dia;
+    int mes;
+    int ano;
+    String descricao;
+    Double valor;
+    ano = Integer.parseInt(linha.substring(6, 10));
+        
+        mes = Integer.parseInt(linha.substring(10, 12));
+        dia = Integer.parseInt(linha.substring(12, 14));
+       conta= parseInt(linha.substring(0,6));
+       descricao = linha.substring(14, 74).trim();
+       valor= parseDouble(linha.substring(74, 86))/100;;
+    Date d=new Date(ano,mes-1,dia);
+    return new Lancamento(conta,d,descricao,valor);
     }
     
     private Lancamento getNextLancamento() throws IOException {
-        throw new UnsupportedOperationException("Não implementado");
+       String linha=getNextLine();
+       
+       if(linha!=null){
+           return processaLinha(linha);
+       }else{
+           return null;
+       }
+        
     }
     
     public List<Lancamento> getLancamentos() throws IOException {
-        throw new UnsupportedOperationException("Não implementado");
+        List<Lancamento> l= new ArrayList<>();
+       Lancamento a;
+        while((a=getNextLancamento())!=null){
+           l.add(a);
+       }
+       reader.close();
+       return l;
+       
     }
     
 }
